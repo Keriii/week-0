cecae8dd177aa17f63b11b97f2bc4bff57a29e35
Kerod Sisay <Kerod53@gmail.com>
2023-11-27 22:22:48 +0300

    updated the loader file

--- src/loader.py
+++ src/loader.py
@@ -98,6 +98,20 @@ def get_community_participation(path):
     return comm_dict
 
 
+def get_messages_from_channel(channel_path):
+    '''
+    get all the messages from a channel        
+    '''
+    channel_json_files = os.listdir(channel_path)
+    channel_msgs = [json.load(open(channel_path + "/" + f)) for f in channel_json_files]
+
+    df = pd.concat([pd.DataFrame(get_messages_dict(msgs)) for msgs in channel_msgs])
+    print(f"Number of messages in channel: {len(df)}")
+    
+    return df
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Export Slack history')
 
